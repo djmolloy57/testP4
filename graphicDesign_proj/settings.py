421d656f50c52575e75c6b4fe6a989ac07b984f9
djmolloy57 <dj_molloy57@hotmail.com>
2022-05-29 16:17:40 +0000

    add auth to settings_py

--- graphicDesign_proj/settings.py
+++ graphicDesign_proj/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    'store',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +73,18 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+
+   # Needed to login by username in Django admin, regardless of 'allauth'
+   'django.contrib.auth.backends.ModelBackend',
+
+   # 'allauth' specific authentication methods, such as login by e-mail
+   'allauth.account.auth_backends.AuthenticationBackend',
+
+)
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'graphicDesign_proj.wsgi.application'
 
